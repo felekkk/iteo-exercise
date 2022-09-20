016eddc4a15517dab1d9018b1187f649ad92325c
Mateusz Felczak <mateuszf@protonmail.com>
2022-09-20 19:55:30 +0200

    feature/1-application-initial-commit

---
+++ docker/bin/console.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -n "$IS_DOCKER" ]; then
+    echo "Script should not be executed in container."
+fi
+
+docker exec -it -u docker iteo-www "bin/console" "$@"
---
+++ docker/bin/fix.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+username="docker"
+
+if [ -n "$IS_DOCKER" ]; then
+    echo "Script should not be executed in container."
+fi
+
+docker exec -it iteo-www chown -R "$username":"$username" ./
