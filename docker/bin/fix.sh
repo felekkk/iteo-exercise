c221e4ed10ab8e99e81260b398b9cf755f2e2c85
Mateusz Felczak <mateuszf@protonmail.com>
2022-09-21 01:29:32 +0200

    feature/3-privileges-for-application-folders-and-doctrine-configuraction

--- docker/bin/fix.sh
+++ docker/bin/fix.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-username="www-data"
+username="docker"
 
 if [ -n "$IS_DOCKER" ]; then
     echo "Script should not be executed in container."
 fi
+
 
 docker exec -it iteo-www chown -R "$username":"$username" ./var/
