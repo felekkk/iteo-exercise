3e8a9bcbe05fe3d41352e651dd27dfc1cee4ec8e
Mateusz Felczak <mateuszf@protonmail.com>
2022-09-21 01:00:15 +0200

    feature/2-encore-react-typectript-configuration

--- docker/bin/npm.sh
+++ docker/bin/npm.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-username="www-data"
-
 if [ -n "$IS_DOCKER" ]; then
     echo "Script should not be executed in container."
 fi
 
-docker exec -it iteo-www chown -R "$username":"$username" ./var/
+docker exec -it -u docker iteo-www npm "$@"
